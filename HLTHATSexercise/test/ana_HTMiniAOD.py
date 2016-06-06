1b041675ee8071aca3366aea32cf342b08ac9ffd
olivito <dominick.olivito@gmail.com>
2016-06-06 03:11:21 -0700

    adding configs/scripts for HT

---
+++ HLTHATSexercise/test/ana_HTMiniAOD.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLTANALYZER')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+#   fileNames = cms.untracked.vstring('/store/data/Run2016B/JetHT/MINIAOD/PromptReco-v2/000/273/730/00000/04C3D8B6-B821-E611-B245-02163E011979.root'),
+   fileNames = cms.untracked.vstring('file:skim_pfht475_5files.root'),
+)
+
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.20 $'),
+    annotation = cms.untracked.string('RelVal nevts:100'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.TFileService = cms.Service("TFileService",
+                                       fileName = cms.string('histos_ht_skim.root')
+                                   )
+
+### analyzer configuration
+
+process.htTrigAnalyzerMiniAOD = cms.EDAnalyzer("HTTrigAnalyzerMiniAOD")
+process.htTrigAnalyzerMiniAOD.refTriggerName = cms.untracked.string("HLT_PFHT475_v2")
+process.htTrigAnalyzerMiniAOD.sigTriggerName = cms.untracked.string("HLT_PFHT800_v2")
+process.htTrigAnalyzerMiniAOD.verbose = cms.untracked.bool(False)
+
+process.GlobalTag.globaltag = "80X_dataRun2_Prompt_v8"
+
+# Path and EndPath definitions
+process.HLTanalyzers = cms.Path(process.htTrigAnalyzerMiniAOD)
---
+++ HLTHATSexercise/test/plot_trigeff_ht.C
@@ -0,0 +1,41 @@
+#include "TFile.h"
+#include "TH1.h"
+#include "TLegend.h"
+#include "TString.h"
+#include "TCanvas.h"
+#include "TEfficiency.h"
+#include "TStyle.h"
+
+void plot_trigeff_ht (const TString& infile = "histos_ht_skim.root") {
+
+  gStyle->SetPadTopMargin(0.08);
+  gStyle->SetPadBottomMargin(0.12);
+  gStyle->SetPadLeftMargin(0.15);
+  gStyle->SetPadRightMargin(0.05);
+
+  TH1::SetDefaultSumw2();
+  
+  TFile* f_in = new TFile(infile);
+
+  // ---- efficiency vs ht
+  
+  TH1F* h_ht_denom = (TH1F*) f_in->Get("htTrigAnalyzerMiniAOD/h_ht_all");
+  TH1F* h_ht_num = (TH1F*) f_in->Get("htTrigAnalyzerMiniAOD/h_ht_passtrig");
+  
+  TCanvas* c_ht = new TCanvas("c_ht","c_ht");
+  c_ht->SetGrid(1,1);
+  c_ht->cd();
+
+  TH2F* h_ht_axis = new TH2F("h_ht_axis",";H_{T} [GeV];Efficiency of HLT_PFHT800",120,0,1200,20,0,1);
+  h_ht_axis->GetYaxis()->SetTitleOffset(0.98);
+  h_ht_axis->Draw();
+  
+  TEfficiency* h_ht_eff = new TEfficiency(*h_ht_num, *h_ht_denom);
+  h_ht_eff->SetLineColor(kRed);
+  h_ht_eff->SetMarkerColor(kRed);
+  
+  h_ht_eff->Draw("pe same");
+  
+ 
+  return;
+}
